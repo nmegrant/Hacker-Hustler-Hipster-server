92ee7e9107b7a9ce67e8821ba548c4524c5b4100
nmegrant <nme.grant@gmail.com>
2020-06-16 18:03:02 +0200

    Add in endpoint for editing homepage-not final

--- routers/homepages.js
+++ routers/homepages.js
@@ -33,4 +33,17 @@ router.get("/homepages/:id", async (request, response, next) => {
   }
 });
 
+// router.patch("/homepages/:id", async (request, response, next) => {
+//   try {
+//     const homepage = await Homepage.findByPk(request.params.id);
+//     if (!homepage) {
+//       return response.status(404).send("Homepage does not exist");
+//     }
+//     await homepage.update({});
+//     response.status(200).send(homepage);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
 module.exports = router;
