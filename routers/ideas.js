01fc50105caee186d89043930dcf39d2571d2a0f
nmegrant <nme.grant@gmail.com>
2020-06-17 12:05:43 +0200

    Set up endpoint to get ideas

---
+++ routers/ideas.js
@@ -0,0 +1,21 @@
+const { Router } = require("express");
+const Idea = require("../models").idea;
+const authMiddleware = require("../auth/middleware");
+
+const router = new Router();
+
+router.get("/ideas", authMiddleware, async (request, response, next) => {
+  try {
+    const ideas = await Idea.findAll({
+      where: { userId: request.user.id },
+    });
+    if (!idea) {
+      return response.status(404).send("Homepage not found");
+    }
+    response.status(200).send(ideas);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
--- routers/mypage.js
+++ routers/mypage.js
@@ -3,7 +3,6 @@ const Homepage = require("../models").homepage;
 const User = require("../models").user;
 const Tag = require("../models").tag;
 const Website = require("../models").website;
-const Idea = require("../models").idea;
 const authMiddleware = require("../auth/middleware");
 
 const router = new Router();
