b146a332933e55ab14ce866a748a0a23a6f76da3
nmegrant <nme.grant@gmail.com>
2020-06-20 12:20:42 +0200

    Finish add error messages and refactor code

--- routers/websites.js
+++ routers/websites.js
@@ -1,9 +1,6 @@
 const { Router } = require("express");
 const Homepage = require("../models").homepage;
-const User = require("../models").user;
-const Tag = require("../models").tag;
 const Website = require("../models").website;
-const Idea = require("../models").idea;
 const authMiddleware = require("../auth/middleware");
 
 const router = new Router();
