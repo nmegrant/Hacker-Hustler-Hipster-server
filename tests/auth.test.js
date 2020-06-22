3ab1b3703ab2ac07442b4dd8b2b9c5d7e3131bc6
nmegrant <nme.grant@gmail.com>
2020-06-22 18:24:42 +0200

    Fix script so schema migrates

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -2,13 +2,28 @@ const app = require("../index");
 const supertest = require("supertest");
 const request = supertest(app);
 
-describe("User routes", () => {
+describe("Auth routes", () => {
   describe("POST /user", () => {
-    test("Should create a user and a homepage", async (done) => {
+    test("POST /signup should fail if missing email || name || password || role", async (done) => {
       const body = {
-        email: "test@test.com",
         name: "Mr. Test",
         password: "1234",
+        role: "Hacker",
+      };
+      const response = await request.post("/signup").send(body);
+      expect(response.status).toBe(400);
+      expect(JSON.parse(response.text)).toEqual({
+        message: "Please supply all sign up information.",
+      });
+      done();
+    });
+  });
+  describe("POST /user", () => {
+    test("POST /signup should create a user and homepage", async (done) => {
+      const body = {
+        email: "test@test.com",
+        password: "1234",
+        name: "Mr. Test",
         role: "Hacker",
       };
       const response = await request.post("/signup").send(body);
