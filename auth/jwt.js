134d0782df5f77e7b7989946f26c840b79c8079d
nmegrant <nme.grant@gmail.com>
2020-06-24 12:35:00 +0200

    Using the || so my tests still pass

--- auth/jwt.js
+++ auth/jwt.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
-const secret = process.env.JWT_SECRET;
+const secret =
+  process.env.JWT_SECRET || "234lj2k3je#$%DFG89rflk345F$45354fgdfgdf45%4$2kld";
 
 function toJWT(data) {
   return jwt.sign(data, secret, { expiresIn: "2h" });
