3dc59b0c5edb797622e57718ce9a995c5b9412b6
nmegrant <nme.grant@gmail.com>
2020-07-06 20:45:45 +0200

    Set up create endpoint

--- routers/favourites.js
+++ routers/favourites.js
@@ -5,7 +5,13 @@ const authMiddleware = require("../auth/middleware");
 const router = new Router();
 
 router.post("/favourites", authMiddleware, async (request, response, next) => {
+  const { favouriteId } = request.body;
   try {
+    await Favourite.create({
+      userId: request.user.userId,
+      favouriteId: favouriteId,
+    });
+    return response.status(201).send();
   } catch (error) {
     next(error);
   }
