2ddca7d109c041692e05a9ae7db5aaea415fac44
nmegrant <nme.grant@gmail.com>
2020-07-06 22:35:08 +0200

    Fix seed for favourites

--- seeders/20200706202656-some-favourites.js
+++ seeders/20200706202656-some-favourites.js
@@ -12,8 +12,8 @@ module.exports = {
           updatedAt: new Date(),
         },
         {
-          userId: 3,
-          favouriteId: 2,
+          userId: 1,
+          favouriteId: 3,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
@@ -30,7 +30,7 @@ module.exports = {
           updatedAt: new Date(),
         },
         {
-          userId: 2,
+          userId: 3,
           favouriteId: 2,
           createdAt: new Date(),
           updatedAt: new Date(),
